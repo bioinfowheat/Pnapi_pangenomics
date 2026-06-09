654ca55713e194b6c6cbaea8df090df97ac9ccbd
chris <maxlongbow@gmail.com>
2026-06-09 10:45:08 +0200

    udpate

---
+++ data.sh
@@ -0,0 +1,45 @@
+# reseq data
+# this data is already cleaned, trimmed, and ready for use
+
+
+
+cd /mnt/griffin/Pierinae_genomes/poolseq_data
+
+######
+# sweden
+Pieris_napi.SWE.15.Lulea_Avan.Field.Thorax.30_1.ctq.fq.gz
+Pieris_napi.SWE.15.Lulea_Avan.Field.Thorax.30_2.ctq.fq.gz
+Pieris_napi.SWE.15.Sundsvall.Field.Thorax.29_1.ctq.fq.gz
+Pieris_napi.SWE.15.Sundsvall.Field.Thorax.29_2.ctq.fq.gz
+Pieris_napi.SWE.15.Umeå.Field.Thorax.30_1.ctq.fq.gz
+Pieris_napi.SWE.15.Umeå.Field.Thorax.30_2.ctq.fq.gz
+
+# season
+Pieris_napi.SWE.2015.Stockholm_SU.Field_midJuly.Abdomen.35_1.ctq.fq.gz
+Pieris_napi.SWE.2015.Stockholm_SU.Field_midJuly.Abdomen.35_2.ctq.fq.gz
+Pieris_napi.SWE.15.Stockholm_SU.Field_earlyAug.Abdomen.30_1.ctq.fq.gz
+Pieris_napi.SWE.15.Stockholm_SU.Field_earlyAug.Abdomen.30_2.ctq.fq.gz
+Pieris_napi.SWE.15.Stockholm_SU.Field_lateAug.Abdomen.36_1.ctq.fq.gz
+Pieris_napi.SWE.15.Stockholm_SU.Field_lateAug.Abdomen.36_2.ctq.fq.gz
+
+# females
+Pieris_napi.SWE.14.Skane_Kullaberg.Field_females.Thorax.24_1.ctq.fq.gz
+Pieris_napi.SWE.14.Skane_Kullaberg.Field_females.Thorax.24_2.ctq.fq.gz
+Pieris_napi.SWE.14.Skane_Kullaberg.Field.Thorax.24_Males_1.ctq.fq.gz
+Pieris_napi.SWE.14.Skane_Kullaberg.Field.Thorax.24_Males_2.ctq.fq.gz
+######
+
+
+
+######
+# other countries
+#
+# spain
+Pieris_napi.ESP.12.Aiguamolls.Field.Thorax.24_1.ctq.fq.gz
+Pieris_napi.ESP.12.Aiguamolls.Field.Thorax.24_2.ctq.fq.gz
+
+
+# chf
+Pieris_napi.CHF.13.StMoritz.Field.Thorax.24_1.ctq.fq.gz
+Pieris_napi.CHF.13.StMoritz.Field.Thorax.24_2.ctq.fq.gz
+
--- genome.sh
+++ genome.sh
@@ -1,19 +1,66 @@
+
+# genome files are here:
+/mnt/griffin/chrwhe/Pieris_napi-GCA_905475465.2/
+
+# if you want to make a link to the genome in your local folder
+ln -s /mnt/griffin/chrwhe/Pieris_napi-GCA_905475465.2/GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna .
+
+# then have a look at the scaffold names
+grep '>' GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna
+
+# the first 10 scaffolds
+head GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna
+
+# stats about the genome
+/data/programs/scripts/AsmQC GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna
+-------------------------------
+    AssemblyQC Result  
+-------------------------------
+Contigs Generated :           42
+Maximum Contig Length : 14,821,532
+Minimum Contig Length :    9,452
+Average Contig Length : 7,599,688.3 ± 6,238,799.0
+Median Contig Length :  11,089,836.0
+Total Contigs Length :  319,186,907
+Total Number of Non-ATGC Characters :      4,900
+Percentage of Non-ATGC Characters :        0.002
+Contigs >= 100 bp :           42
+Contigs >= 200 bp :           42
+Contigs >= 500 bp :           42
+Contigs >= 1 Kbp :            42
+Contigs >= 10 Kbp :           41
+Contigs >= 1 Mbp :            27
+N50 value :     13,068,865
+Generated using /mnt/griffin/chrwhe/Pieris_napi-GCA_905475465.2/GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna
+
+
+
+##############
+# details
+##############
+
 # NCBI
 https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/905/475/465/GCF_905475465.1_ilPieNapi1.2/
 
+# if you want to download
+
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/905/475/465/GCF_905475465.1_ilPieNapi1.2/GCF_905475465.1_ilPieNapi1.2_genomic.fna.gz
 gunzip GCF_905475465.1_ilPieNapi1.2_genomic.fna.gz
+# make an index
 samtools faidx GCF_905475465.1_ilPieNapi1.2_genomic.fna
 
 
-# Bedtools needs an index of our scaffolds and their lengths:
-/data/programs/exonerate-2.2.0/src/util/fastalength GCF_905475465.1_ilPieNapi1.2_genomic.fna | awk 'BEGIN {FS=" "}{print $2,"\t",$1}' > GCF_905475465.1_ilPieNapi1.2.bedgenome
+grep '>' GCF_905475465.1_ilPieNapi1.2_genomic.fna
+>NC_062234.1 Pieris napi chromosome 1, ilPieNapi1.2, whole genome shotgun sequence
+>NC_062235.1 Pieris napi chromosome 2, ilPieNapi1.2, whole genome shotgun sequence
+>NC_062236.1 Pieris napi chromosome 3, ilPieNapi1.2, whole genome shotgun sequence
+>NC_062237.1 Pieris napi chromosome 4, ilPieNapi1.2, whole genome shotgun sequence
 
-head /mnt/griffin/chrwhe/Pieris_napi-GCA_905475465.2/GCF_905475465.1_ilPieNapi1.2.bedgenome
-NC_062234.1      14821532
-NC_062235.1      14218065
-NC_062236.1      13690485
-NC_062237.1      13663050
-NC_062238.1      13535449
 
+# I have renamed the genome scaffolds
+grep '>' /mnt/griffin/chrwhe/Pieris_napi-GCA_905475465.2/GCF_905475465.1_ilPieNapi1.2_genomic.renamed.fna
+>1
+>2
+>3
+>4
 
